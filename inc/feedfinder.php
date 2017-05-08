38c0a5149fee8b076fa7cf626bb44e783f089dd7
spip.franck@lien-d-amis.net <>
2017-05-08 12:04:22 +0000

    contrib.spip est maintenant en https, donc, j'ajoute le "s" à http

--- inc/feedfinder.php
+++ inc/feedfinder.php
@@ -88,7 +88,7 @@ function is_feed($url){
 }
 
 /*****************test is_feed******************************
-echo is_feed("http://contrib.spip.net/spip.php?page=backend" _EXTENSIO_PHP") . "<br />"; //retourne rss
+echo is_feed("https://contrib.spip.net/spip.php?page=backend" _EXTENSIO_PHP") . "<br />"; //retourne rss
 echo is_feed("http://liberation.fr/rss.php") . "<br />"; //retourne rss
 echo is_feed("http://liberation.fr/rss.php") . "<br />"; //retourne rss
 echo is_feed("http://willy.boerland.com/myblog/atom/feed") //retourne atom
@@ -185,7 +185,7 @@ print_r (get_feed_from_url("http://willy.boerland.com/myblog/"));
 *****************************    Resultat *****************************************
 Array
 (
-    [0] => http://contrib.spip.net/backend.php
+    [0] => https://contrib.spip.net/backend.php
 )
 Array
 (
