477056f953a14ddfc8a210afbc0e7d99f08a7de8
b_b <bruno@eliaz.fr>
2021-11-23 11:20:41 +0100

    des placeholder en https plutôt que préremplir des champ avec http://
    report adapté de https://git.spip.net/spip/medias/commit/102b361cee1511fdace246ff74c422404ed2a1be & fix #4837
    
    (cherry picked from commit 6486f4245450c1287afbfadb110ab6babc75fbcd)

--- formulaires/editer_site_fonctions.php
+++ formulaires/editer_site_fonctions.php
@@ -17,7 +17,7 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 function saisie_url_syndic($url_syndic, $name = 'url_syndic', $id = 'url_syndic') {
 	$res = "";
 	if (strlen($url_syndic) < 8) {
-		$url_syndic = "http://";
+		$url_syndic = '';
 	}
 
 	// cas d'une liste de flux detectee par feedfinder : menu
@@ -29,7 +29,7 @@ function saisie_url_syndic($url_syndic, $name = 'url_syndic', $id = 'url_syndic'
 		}
 		$res .= "</select>\n";
 	} else {
-		$res .= "<input type='text' class='text' name='$name' id='$id' value=\"$url_syndic\" size='40' />\n";
+		$res .= "<input type='text' class='text' name='$name' id='$id' placeholder='https://' value=\"$url_syndic\" size='40' />\n";
 	}
 
 	return $res;
